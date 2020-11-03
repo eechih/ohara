c6232c3860e599f8a552133d06f6798c21104b56
Chih-Chiang Yeh <eechih@gmail.com>
2020-11-03 15:59:51 +0800

    use Array.prototype.reverse() instead

--- ohara-manager/client/src/components/common/LogViewer.tsx
+++ ohara-manager/client/src/components/common/LogViewer.tsx
@@ -1,5 +1,21 @@
+/*
+ * Copyright 2019 is-land
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React, { memo } from 'react';
-import { forEachRight, map, times } from 'lodash';
+import { map, times } from 'lodash';
 import Skeleton from '@material-ui/lab/Skeleton';
 import Box from '@material-ui/core/Box';
 
@@ -8,17 +24,8 @@ interface LogViewerProps {
   loading?: boolean;
 }
 
-function reverse(logs: string[]): string[] {
-  const reverseLogs: string[] = [];
-  forEachRight(logs, (log: string) => {
-    reverseLogs.push(log);
-  });
-  return reverseLogs;
-}
-
 const LogViewer: React.FC<LogViewerProps> = (props) => {
   const { logs = [], loading = true } = props;
-  const reverseLogs = reverse(logs);
 
   if (loading) {
     return (
@@ -40,7 +47,7 @@ const LogViewer: React.FC<LogViewerProps> = (props) => {
       overflow="auto"
     >
       <Box flexGrow={1} key={-1} />
-      {map(reverseLogs, (log: string, index: number) => {
+      {map(logs.reverse(), (log: string, index: number) => {
         return (
           <Box data-testid="log" key={index}>
             {log}
